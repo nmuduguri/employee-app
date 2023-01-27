8ce2866010a6e51ab834f09badf08f6a78b70a5e
nagaraju <nmuduguri>
2023-01-27 08:06:53 +0530

    Revert "code changes for sysout and dependency"
    This reverts commit a52ed3ca2b1b6cf05f6c778a7b0a0d8e6fedb6e4.

--- employee/src/main/java/com/employee/EmployeeApplication.java
+++ employee/src/main/java/com/employee/EmployeeApplication.java
@@ -8,8 +8,6 @@ public class EmployeeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
-		
-		System.out.println("Employee Application Started..");
 	}
 
 }
